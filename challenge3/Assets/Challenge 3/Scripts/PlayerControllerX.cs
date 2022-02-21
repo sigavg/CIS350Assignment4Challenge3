689e28f0486f754a6d794e8ed8f644d42ca5ab9d
sigavg <lesimplu@gmail.com>
2022-02-20 19:14:20 -0600

    Challenge 3

---
+++ challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,84 @@
+﻿/* Luke Lesimple
+ * Challenge 4
+ * Controls player movement EDITED
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    public bool canfloat = true;
+
+    public float floatForce = 1.0f;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+    public AudioClip bounceSound;
+
+    private UImanager ui;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui = GameObject.FindObjectOfType<UImanager>();
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && canfloat)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+            ui.score++;
+        }
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.velocity = new Vector3(0, 0, 0); // set y velocity to zero
+            playerRb.AddForce(new Vector3(0, 75000, 0)); // some constant force here
+
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
+
+    }
+
+}
---
+++ challenge3/Assets/Challenge 3/Scripts/UImanager.cs
@@ -0,0 +1,71 @@
+﻿/* Luke Lesimple
+ * Challenge 3
+ * manages UI
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+
+    public PlayerControllerX pc;
+
+    public bool won = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+
+        if (pc == null)
+        {
+            pc = GameObject.FindGameObjectWithTag("Balloon").GetComponent<PlayerControllerX>();
+        }
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pc.gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        if (pc.gameOver && !won)
+        {
+            scoreText.text = "You Lose!" + "\n" +
+                             "Press R to Try Again";
+        }
+
+        if (score >= 10)
+        {
+            pc.gameOver = true;
+            won = true;
+
+            // TODO: stop player running
+            //pc.StopRunning();
+
+            scoreText.text = "You Win!" + "\n" +
+                             "Press R to Try Again";
+        }
+
+        if (pc.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+
+    }
+}
